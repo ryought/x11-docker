d878ba5b38b9662140a01992e7c9c23546b8da50
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2019-07-11 12:24:34 +0900

    driverの中身を探検

---
+++ driver_inspect/download.sh
@@ -0,0 +1,6 @@
+wget https://us.download.nvidia.com/tesla/418.40.04/NVIDIA-Linux-x86_64-418.40.04.run
+
+
+
+
+./nvidia-installer --silent --no-kernel-module --install-compat32-libs --no-nouveau-check --no-nvidia-modprobe --no-rpms --no-backup --no-check-for-alternate-installs --no-libglx-indirect --no-install-libglvnd --no-glvnd-egl-client --no-glvnd-glx-client  --x-prefix=/tmp/null --x-module-path=/tmp/null --x-library-path=/tmp/null --x-sysconfig-path=/tmp/null
