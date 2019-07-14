9b17f50bb295813c90325b1695663b8589752659
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2019-07-14 12:56:44 +0900

    update

--- gl_on_docker/run.sh
+++ gl_on_docker/run.sh
@@ -1,12 +1,16 @@
 # inside docker script
 
+# 0. generate xorg.conf if not copied
+[ ! -e /etc/X11/xorg.conf ] && nvidia-xconfig -a --virtual=$SCREEN_RESOLUTION --allow-empty-initial-configuration --enable-all-gpus --busid $BUSID
+
 # 1. launch X server
 Xorg :0 &
-sleep 5
+sleep 5  # wait for the server gets ready
 
 # 2. start x11 and vnc connection
-x11vnc -display :0 -passwd ryought -forever &
+x11vnc -display :0 -passwd $VNC_PASSWORD -forever &
 
 # 3. start simulator
-#DISPLAY=:0 ./lg/simulator -screen-height 480 -screen-width 640 -screen-quality Beautiful -screen-fullscreen 0
-DISPLAY=:0 ./lg2/simulator
+DISPLAY=:0
+./lg/simulator -screen-height 480 -screen-width 640 -screen-quality Beautiful -screen-fullscreen 0
+# ./lg2/simulator
