66dbfbf967f65d1dc34a82acc9508232ecb9a681
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2019-07-14 13:01:57 +0900

    update

--- gl_on_docker/run.sh
+++ gl_on_docker/run.sh
@@ -5,10 +5,11 @@
 
 # 1. launch X server
 Xorg :0 &
-sleep 5  # wait for the server gets ready
+sleep 3  # wait for the server gets ready
 
 # 2. start x11 and vnc connection
 x11vnc -display :0 -passwd $VNC_PASSWORD -forever &
+sleep 3  # wait for the server gets ready
 
 # 3. start simulator
 DISPLAY=:0
